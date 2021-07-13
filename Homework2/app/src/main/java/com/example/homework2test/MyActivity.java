b2eab6568c66bfd79d06edefe3a7f0326bd00cc7
longxiang-ai <1425507157@qq.com>
2021-07-13 15:28:33 +0800

    with dial

--- Homework2/app/src/main/java/com/example/homework2test/MyActivity.java
+++ Homework2/app/src/main/java/com/example/homework2test/MyActivity.java
@@ -2,7 +2,10 @@ package com.example.homework2test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,12 +16,36 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-//        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button btnToPractice = findViewById(R.id.button1);  // 找到button 1
+        btnToPractice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MyActivity.this,"button clicked",Toast.LENGTH_SHORT).show();
+                // 点击button 1 跳转至 practice page
+                Log.i("MyActivity:","尝试进入PracActivity");
+                Intent intent_to_practice = new Intent(MyActivity.this,PracticeActivity.class);
+                startActivity(intent_to_practice);
+            }
+        });
+        Button btnToBaidu = findViewById(R.id.button2);  // 找到button 2
+        btnToBaidu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 点击button 1 跳转至 practice page
+                Log.i("MyActivity:","尝试打开百度");
+                Intent intent_to_Baidu = new Intent(Intent.ACTION_VIEW);
+                intent_to_Baidu.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent_to_Baidu);
+            }
+        });
+        Button btnToDial = findViewById(R.id.button3);  // 找到button 3
+        btnToDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 点击button 1 跳转至 practice page
+                Log.i("MyActivity:","尝试拨号");
+                Intent intent_to_Dial = new Intent(Intent.ACTION_CALL,Uri.parse("tel:10086"));
+//                intent_to_Dial.setData(Uri.parse("tel:10086"));
+                startActivity(intent_to_Dial);
             }
         });
     }
--- Homework2/app/src/main/java/com/example/homework2test/PracticeActivity.java
+++ Homework2/app/src/main/java/com/example/homework2test/PracticeActivity.java
@@ -3,6 +3,10 @@ package com.example.homework2test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PracticeActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class PracticeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+        Button btn1 = findViewById(R.id.button_toast);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("PracticeActivity:","尝试toast功能");
+                Toast.makeText(PracticeActivity.this,"button clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
+
 }
