b7a3fcc304f3f917f2c495928468f52d2a917e58
longxiang-ai <1425507157@qq.com>
2021-07-13 14:44:37 +0800

    homework2

---
+++ Homework2/app/src/main/java/com/example/homework2test/MyActivity.java
@@ -0,0 +1,26 @@
+package com.example.homework2test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+//        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MyActivity.this,"button clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
