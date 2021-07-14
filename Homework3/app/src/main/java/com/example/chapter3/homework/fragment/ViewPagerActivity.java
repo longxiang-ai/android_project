758fd6edec080b2382c201bc54d1a035f14d6605
longxiang-ai <1425507157@qq.com>
2021-07-14 15:10:23 +0800

    homework3

---
+++ Homework3/app/src/main/java/com/example/chapter3/homework/fragment/ViewPagerActivity.java
@@ -0,0 +1,31 @@
+package com.example.chapter3.homework.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.chapter3.homework.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+        ViewPager pager = findViewById(R.id.view_pager);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return new ChatFragment();
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+    }
+}
