ccc0d4bc23f02bd45ee9c983cbf9f349d4445519
longxiang-ai <1425507157@qq.com>
2021-07-20 14:48:07 +0800

    增加edittext输入URL功能

--- Homework7/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
+++ Homework7/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.mediaworksdemo;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -25,19 +26,26 @@ public class PictureDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_picture_detail);
 
         ImageView imageView = findViewById(R.id.iv_detail);
+        EditText textInput = findViewById(R.id.et_input);
         Button btnSuccess = findViewById(R.id.btn_load_success);
         Button btnFail = findViewById(R.id.btn_load_fail);
         Button btnRoundedCorners = findViewById(R.id.btn_rounded_corners);
 
         btnSuccess.setOnClickListener( v -> {
-            Glide.with(this).load(mockUrl)
+            String text = textInput.getText().toString();
+            if (text.length() == 0)
+                text = mockUrl;
+            Glide.with(this).load(text)
                     .placeholder(R.drawable.loading_green)
                     .error(R.drawable.error_red)
                     .into(imageView);
         });
 
         btnFail.setOnClickListener( v -> {
-            Glide.with(this).load(mockErrorUrl)
+            String text = textInput.getText().toString();
+            if (text.length() == 0)
+                text = mockErrorUrl;
+            Glide.with(this).load(text)
                     .placeholder(R.drawable.loading_green)
                     .error(R.drawable.error_red)
                     .into(imageView);
